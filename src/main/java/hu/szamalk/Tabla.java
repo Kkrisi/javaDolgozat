2509b319ef706b5c3d986d609bb1a95cea5a90ce
Kkrisi <kadarkrisi@gmail.com>
2024-12-09 09:20:09 +0100

    f1 1

---
+++ src/main/java/hu/szamalk/Tabla.java
@@ -0,0 +1,35 @@
+package hu.szamalk;
+
+public class Tabla {
+    private String[] T;
+    private String[] UresCella;
+
+    public Tabla(String[] t, String[] uresCella) {
+        T = t;
+        UresCella = uresCella;
+    }
+
+    public void Elhelyez(){
+
+    }
+
+    public void FaljbaIr(){
+
+    }
+
+    public void Megjelenit(){
+
+    }
+
+    public void Tabla(){
+
+    }
+
+    public void UresOszlop(){
+
+    }
+
+    public void UresSor(){
+
+    }
+}
